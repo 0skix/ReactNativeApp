cd0d1f9fcc52d97faefea94ff3a0b05c1ad6dc06
0skix <oskyzera@gmail.com>
2021-06-18 18:19:50 +0200

    oj tak

--- contexts/CoursesContext.js
+++ contexts/CoursesContext.js
@@ -3,13 +3,13 @@ export const CoursesContext = createContext();
 export const CoursesContextProvider = ({ children }) => {
 	const [progress, setProgress] = useState([0, 0, 0, 0, 0, 0]);
 	const [checked, setChecked] = useState(false);
-	const [cSmsPhishing, setcSmsPhishing] = useState({
-		a: false,
-		b: false,
-		c: false,
-		d: false,
-		e: false,
-	});
+	const [cSmsPhishing, setcSmsPhishing] = useState([
+		false,
+		false,
+		false,
+		false,
+		false,
+	]);
 	const value = {
 		progress,
 		setProgress,
