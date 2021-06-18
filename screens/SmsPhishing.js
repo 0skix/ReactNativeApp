d542cf8725ae0824b21835c2f5f1c039b814568d
0skix <oskyzera@gmail.com>
2021-06-18 18:19:34 +0200

    udalo sie

--- screens/SmsPhishing.js
+++ screens/SmsPhishing.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, FlatList } from "react-native";
 import { Button, Overlay, Card } from "react-native-elements";
 import { CheckBox } from "react-native-elements/dist/checkbox/CheckBox";
 import { CoursesContext } from "../contexts/CoursesContext";
@@ -15,186 +15,76 @@ const SmsPhishing = () => {
 		setcSmsPhishing,
 	} = useContext(CoursesContext);
 
-	useEffect(() => {
-		console.log(cSmsPhishing);
-	}, [cSmsPhishing]);
+	const text = [
+		"• SMS phishing – atak socjotechniczny polegający na rozsyłaniu SMS-ów, które mają skłonić ofiarę do podjęcia określonego działania.",
+		"	• Przykładem SMS phishingu jest wiadomość w której kurier prosi o niewielką dopłatę do przesyłki w celu szybszego jej dostarczenia.",
+		"	• W przypadku SMS phishingu cyberprzestępcy używają wiadomościtekstowych, aby skłonić potencjalne ofiary do podania danych osobowych.",
+		"	• Wiadomości tekstowe zazwyczaj zawierają linki do podrobionych stron, które wyglądają identycznie jak strony banków czy innych stron, które znamy. Zdobywają tym samym nasze zaufanie i chętniej podajemy swoje dane, które następnie są wykorzystywane do niecnych celów hakerów.",
+		"• SmsPhishing zyskał popularność wśród przestępców, gdy ż stale rosnąca popularność smartfonów umożliwia im kradzież poufnych danych bez konieczności przełamywania zabezpieczeń komputera lub sieci.",
+	];
 	return (
 		<ScrollView>
 			<Card>
 				<Card.Title>
 					Zapoznaj się z atakiem typu SmsPhishing{" "}
-					{cSmsPhishing.a === true ? "true" : "false"}
-					{cSmsPhishing.b === true ? "true" : "false"}
+					{cSmsPhishing[0] === true ? "true" : "false"}{" "}
+					{cSmsPhishing[1] === true ? "true" : "false"}{" "}
+					{cSmsPhishing[2] === true ? "true" : "false"}{" "}
+					{cSmsPhishing[3] === true ? "true" : "false"}{" "}
+					{cSmsPhishing[4] === true ? "true" : "false"}
 				</Card.Title>
-				<Card.Divider />
-				<StyledText1 style={{ marginBottom: 5 }}>
-					• SMS phishing – atak socjotechniczny polegający na rozsyłaniu SMS-ów,
-					które mają skłonić ofiarę do podjęcia określonego działania.
-				</StyledText1>
-				<CheckBox
-					center
-					title="Odznacz sekcje"
-					checkedIcon="dot-circle-o"
-					uncheckedIcon="circle-o"
-					iconRight
-					checked={cSmsPhishing.a}
-					onPress={() => {
-						if (cSmsPhishing.a === false) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, a: true };
-							});
-							setProgress((prevState) => {
-								return { ...prevState, whaling: prevState.whaling + 0.2 };
-							});
-						} else if (cSmsPhishing.a === true) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, a: false };
-							});
-							setProgress((prevState) => {
-								return {
-									...prevState,
-									whaling: prevState.whaling - 0.2,
-								};
-							});
-						}
-					}}
-				/>
-				<Card.Divider />
-				<StyledText1 style={{ marginBottom: 15 }}>
-					• Przykładem SMS phishingu jest wiadomość w której kurier prosi o
-					niewielką dopłatę do przesyłki w celu szybszego jej dostarczenia.
-				</StyledText1>
-				<CheckBox
-					center
-					title="Odznacz sekcje"
-					checkedIcon="dot-circle-o"
-					uncheckedIcon="circle-o"
-					iconRight
-					checked={cSmsPhishing.b}
-					onPress={() => {
-						if (cSmsPhishing.b === false) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, b: true };
-							});
-							setProgress((prevState) => {
-								return { ...prevState, whaling: prevState.whaling + 0.2 };
-							});
-						} else if (cSmsPhishing.b === true) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, b: false };
-							});
-							setProgress((prevState) => {
-								return {
-									...prevState,
-									whaling: prevState.whaling - 0.2,
-								};
-							});
-						}
-					}}
-				/>
 
-				<Card.Divider />
-				<StyledText1 style={{ marginBottom: 15 }}>
-					• W przypadku SMS phishingu cyberprzestępcy używają wiadomości
-					tekstowych, aby skłonić potencjalne ofiary do podania danych
-					osobowych.
-				</StyledText1>
-				<CheckBox
-					center
-					title="Odznacz sekcje"
-					checkedIcon="dot-circle-o"
-					uncheckedIcon="circle-o"
-					iconRight
-					checked={cSmsPhishing.c}
-					onPress={() => {
-						if (cSmsPhishing.c === false) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, c: true };
-							});
-							setProgress((prevState) => {
-								return { ...prevState, whaling: prevState.whaling + 0.2 };
-							});
-						} else if (cSmsPhishing.c === true) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, c: false };
-							});
-							setProgress((prevState) => {
-								return {
-									...prevState,
-									whaling: prevState.whaling - 0.2,
-								};
-							});
-						}
-					}}
-				/>
-				<Card.Divider />
-				<StyledText1 style={{ marginBottom: 15 }}>
-					• Wiadomości tekstowe zazwyczaj zawierają linki do podrobionych stron,
-					które wyglądają identycznie jak strony banków czy innych stron, które
-					znamy. Zdobywają tym samym nasze zaufanie i chętniej podajemy swoje
-					dane, które następnie są wykorzystywane do niecnych celów hakerów.
-				</StyledText1>
-				<CheckBox
-					center
-					title="Odznacz sekcje"
-					checkedIcon="dot-circle-o"
-					uncheckedIcon="circle-o"
-					iconRight
-					checked={cSmsPhishing.d}
-					onPress={() => {
-						if (cSmsPhishing.d === false) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, d: true };
-							});
-							setProgress((prevState) => {
-								return { ...prevState, whaling: prevState.whaling + 0.2 };
-							});
-						} else if (cSmsPhishing.d === true) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, d: false };
-							});
-							setProgress((prevState) => {
-								return {
-									...prevState,
-									whaling: prevState.whaling - 0.2,
-								};
-							});
-						}
-					}}
-				/>
-				<Card.Divider />
-				<StyledText1 style={{ marginBottom: 15 }}>
-					• SmsPhishing zyskał popularność wśród przestępców, gdy ż stale
-					rosnąca popularność smartfonów umożliwia im kradzież poufnych danych
-					bez konieczności przełamywania zabezpieczeń komputera lub sieci.
-				</StyledText1>
-				<CheckBox
-					center
-					title="Odznacz sekcje"
-					checkedIcon="dot-circle-o"
-					uncheckedIcon="circle-o"
-					iconRight
-					checked={cSmsPhishing.e}
-					onPress={() => {
-						if (cSmsPhishing.e === false) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, e: true };
-							});
-							setProgress((prevState) => {
-								return { ...prevState, whaling: prevState.whaling + 0.2 };
-							});
-						} else if (cSmsPhishing.e === true) {
-							setcSmsPhishing((prevState) => {
-								return { ...prevState, e: false };
-							});
-							setProgress((prevState) => {
-								return {
-									...prevState,
-									whaling: prevState.whaling - 0.2,
-								};
-							});
-						}
-					}}
+				<FlatList
+					data={text}
+					renderItem={({ item, index }) => (
+						<>
+							<Card.Divider />
+							<StyledText1 style={{ marginBottom: 5 }}>
+								{item} {progress[1]}{" "}
+							</StyledText1>
+							<CheckBox
+								center
+								title="Odznacz sekcje"
+								checkedIcon="dot-circle-o"
+								uncheckedIcon="circle-o"
+								iconRight
+								checked={cSmsPhishing[index]}
+								onPress={() => {
+									if (cSmsPhishing[index] === false) {
+										setcSmsPhishing((prevState) => {
+											let result = prevState;
+											result[index] = true;
+											return result;
+										});
+										setProgress((prevState) => {
+											let result = prevState;
+											result[1] = cSmsPhishing.reduce(
+												(acc, curr) => (curr === true ? acc + 0.2 : acc),
+												0
+											);
+											return [...result];
+										});
+									} else if (cSmsPhishing[index] === true) {
+										setcSmsPhishing((prevState) => {
+											let result = prevState;
+											result[index] = false;
+											return result;
+										});
+										setProgress((prevState) => {
+											let result = prevState;
+											result[1] = cSmsPhishing.reduce(
+												(acc, curr) => (curr === true ? acc + 0.2 : acc),
+												0
+											);
+											return [...result];
+										});
+									}
+								}}
+							/>
+							<Card.Divider />
+						</>
+					)}
+					keyExtractor={(item) => item.id}
 				/>
 			</Card>
 		</ScrollView>
